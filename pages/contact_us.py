c7d0b3ffd706c7540b5cfbbc0683cfe8cdff0ddf
Jaquan Rhodes <jaquanrhodes@gmail.com>
2024-01-27 17:17:59 -0600

    sent email using form

--- pages/contact_us.py
+++ pages/contact_us.py
@@ -4,11 +4,16 @@ st.header("Contact Me")
 
 with st.form(key="email_form"):
     user_email = st.text_input("Your email address")
-    message = st.text_area("Your Message")
-    message = message + "\n" + user_email
+    raw_message = st.text_area("Your Message")
+    message = f"""\
+    Subject: New Email from {user_email}
+    From: {user_email} 
+    {raw_message}
+    """
     button = st.form_submit_button("Submit")
     if button:
         send_email(message)
+        st.info("your email was sent successfully!")
 
 
 
